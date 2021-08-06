99548b4bba61965db5384f5c31f318d5dd74ab96
Lindrew <spongebobrafael@gmail.com>
2021-08-06 21:55:16 +0800

    Added menu contents

--- src/Checkmark/CheckmarkMenu.cs
+++ src/Checkmark/CheckmarkMenu.cs
@@ -1,15 +1,43 @@
+using System;
+using static System.Console;
+using Sharprompt;
+
 namespace Checkmark
-{
+{ 
     public class CheckmarkMenu
     {
+        private const string prefix = ">>"; 
         public static void Run()
         {
-            /*
-            * Create
-            * Read
-            * Update
-            * Delete
-            */
+            while(true)
+            {    
+                ShowMenu();
+            }
+        }
+        private static void ShowMenu()
+        {
+            Write(prefix);
+
+            WriteLine(" Welcome.");
+            
+            var input = Prompt.Select("What do you wish to do?", new[] { "Create", "Read", "Update" });
+            
+            switch(input.ToUpper())
+            {
+                case "CREATE":
+                    WriteLine("Creating...");
+                    break;
+                case "READ":
+                    WriteLine("Reading...");
+                    break;
+                case "UPDATE": 
+                    Environment.Exit(0);
+                    break;
+                case "DELETE":
+                    WriteLine("Error...");
+                    break;
+            }
+            Environment.Exit(0);
         }
     }
 }
