fbe29407b9effc3326fb4022c4c91f3edcc4bb6c
Lindrew <spongebobrafael@gmail.com>
2021-08-08 01:31:16 +0800

    removed extra new lines

--- src/Checkmark/ListItem.cs
+++ src/Checkmark/ListItem.cs
@@ -2,9 +2,7 @@ namespace Checkmark
 {
     public class ListItem
     {
-        public string Todo { get; set; }
+        public string ItemName { get; set; }
         public string PriorityLevel { get; set; }
-        
-        
     }
 }
