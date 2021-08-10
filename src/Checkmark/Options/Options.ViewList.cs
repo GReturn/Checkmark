6d36acda6e93c2c5c7a3c1e8f7e16fc58235f68c
Lindrew <spongebobrafael@gmail.com>
2021-08-10 23:07:07 +0800

    added System.IO

--- src/Checkmark/Options/Options.ViewList.cs
+++ src/Checkmark/Options/Options.ViewList.cs
@@ -1,9 +1,12 @@
+using System.IO;
+
 namespace Checkmark
 {
     public partial class Options
     {
         public void ViewList()
         {
+            // if (File.GetDirectory(PATH)) {}
             System.Console.WriteLine(PATH);
         }
         private void ReadList()
