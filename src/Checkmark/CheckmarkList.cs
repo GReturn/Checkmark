8b8b21f1f1870874247cf547a29d3c5f8f5c83b1
Lindrew <spongebobrafael@gmail.com>
2021-08-11 16:37:45 +0800

    Added full input prompt

--- src/Checkmark/CheckmarkList.cs
+++ src/Checkmark/CheckmarkList.cs
@@ -1,28 +1,11 @@
+using System.Collections.Generic;
+
 namespace Checkmark
 {
     public class CheckmarkList
     {
         public string ListTitle { get; set; }
         public string Priority { get; set; }
-        public string[] Items { get; set; }
-        
-        /*
-        TODO: Concept: Generate a new json file:
-        
-        my_lists.json
-        {
-            "ListTitleHere": {
-                "Priority": "insertPriority",
-                "Items": [
-                    "[] DoThis1",
-                    "[] Do This2"
-                ]
-            }
-            "AnotherListHere": {}
-        }
-        Looks good.
-        */
-        
-        
+        public IEnumerable<string> Items { get; set; }
     }
 }
--- src/Checkmark/Options/Options.CreateList.cs
+++ src/Checkmark/Options/Options.CreateList.cs
@@ -1,7 +1,9 @@
+using static System.Console;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Sharprompt;
+using System.Collections.Generic;
 
 namespace Checkmark
 {
@@ -11,30 +13,35 @@ namespace Checkmark
         {
             ShowCreateMenu();
         }
-        private void ShowCreateMenu()
+        private static void ShowCreateMenu()
         {
             var inputTitle = Prompt.Input<string>("Input title of list");
+
+            var inputItems = Prompt.List<string>("Please add item(s) to your list");
+
+            var inputPriority = Prompt.Input<string>("Input title of list");
+
+
             var answer = Prompt.Confirm($"Are you sure to create {inputTitle}?");
-            //var newList = CheckmarkList;
             
             if (answer)
             {
-                // Skips to file creation if directory exists (does not overwrite):
+                // Skips to file creation if directory exists (does not overwrite)
                 Directory.CreateDirectory(DIR);
 
-                _ = GenerateFile(inputTitle);
+                _ = GenerateFile(inputTitle, inputPriority, inputItems);
             }
         }
 
-        private static async Task GenerateFile(string title)
+        private static async Task GenerateFile(string title, string priority, IEnumerable<string> items)
         {
             var fileName = $"{title}.json";
             var pathToFileName = Path.Combine(DIR, fileName);
             var newList = new CheckmarkList
             {
                 ListTitle = title,
-                Priority = null,
-                Items = null
+                Priority = priority,
+                Items = items
             };
             using var fs = File.Create(pathToFileName);
             await JsonSerializer.SerializeAsync(fs, newList);
