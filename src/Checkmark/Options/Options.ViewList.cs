c166600314ae6f782f78467f3c9363faaa726fa3
Lindrew <spongebobrafael@gmail.com>
2021-08-11 21:52:13 +0800

    Added JSON file read mechanic (in-progress)

--- src/Checkmark/Options/Options.ViewList.cs
+++ src/Checkmark/Options/Options.ViewList.cs
@@ -1,4 +1,7 @@
 using System.IO;
+using System.Text.Json;
+using static System.Console;
+using System.Text;
 
 namespace Checkmark
 {
@@ -6,12 +9,36 @@ namespace Checkmark
     {
         public void ViewList()
         {
-            // if (File.GetDirectory(PATH)) {}
-            System.Console.WriteLine(DIR);
+            ReadList();
         }
         private void ReadList()
         {
+            var listsOfTasks = Deserialize(PATH);
+            WriteLine(listsOfTasks);
+        }
 
+        private void ReadFile(string file)
+        {
+            using var fileString = File.OpenRead(PATH);
+            using var streamReader = new StreamReader(fileString, new UTF8Encoding(false));
+            file = streamReader.ReadToEnd();
+        }
+
+        private bool isFileExisting()
+        {
+            return true;
+        }
+
+        private static CheckmarkList Deserialize(string text)
+        {
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                WriteIndented = true
+            };
+            var json = ReadFile(text);
+
+            return JsonSerializer.Deserialize<CheckmarkList>(json, options);
         }
     }
 }
