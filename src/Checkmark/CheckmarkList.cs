4a7236b45abe74ca73795fa764de0b9fbe15fc56
Lindrew <spongebobrafael@gmail.com>
2021-08-11 21:51:13 +0800

    Added JsonPropertyAttributes

--- src/Checkmark/CheckmarkList.cs
+++ src/Checkmark/CheckmarkList.cs
@@ -1,11 +1,33 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Checkmark
 {
     public class CheckmarkList
     {
+        [JsonPropertyName("ListTitle")]
         public string ListTitle { get; set; }
+        [JsonPropertyName("Priority")]
         public string Priority { get; set; }
+        [JsonPropertyName("Items")]
         public IEnumerable<string> Items { get; set; }
+
+        /*
+        {
+            Title: MyList1,
+            Priority: sddnsd,
+            Items: [
+                item1,
+                item2,
+                item3
+            ]
+        }
+        {
+            Title: MyList2,
+            Priority: dsndjsdsnd,
+            Items: []
+        }
+        
+        */
     }
 }
