0d21ecd5d719edb6da02e79ea864cb65bb4fe977
Lindrew <spongebobrafael@gmail.com>
2021-08-14 19:59:57 +0800

    created CheckmarkConfig.cs

---
+++ src/Checkmark/CheckmarkConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Checkmark
+{
+    public class CheckmarkConfig
+    {
+        public static string Directory { get; } = "/LocalStorage/";
+        public static string Filename { get; } = "checkmark-config.json";
+        public static string PATH { get; } = Path.Combine(Directory, Filename);
+    }
+}
