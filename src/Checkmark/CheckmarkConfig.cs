73acbc6cba64d800b88c587b7e978d3d05e4f01a
Lindrew <spongebobrafael@gmail.com>
2021-08-15 00:29:33 +0800

    Set properties

--- src/Checkmark/CheckmarkConfig.cs
+++ src/Checkmark/CheckmarkConfig.cs
@@ -1,11 +1,16 @@
-﻿using System.IO;
+﻿using System.Text.Json.Serialization;
 
 namespace Checkmark
 {
     public class CheckmarkConfig
     {
-        public static string Directory { get; } = "/LocalStorage/";
-        public static string Filename { get; } = "checkmark-config.json";
-        public static string PATH { get; } = Path.Combine(Directory, Filename);
+        // Path location of checkmark-config.json
+        public string CheckmarkConfigPath { get; } = "LocalStorage/checkmark-config.json";
+
+        // Contents of checkmark-config.json
+        [JsonPropertyName("Directory")]
+        public string DIR { get; set; }
+        [JsonPropertyName("Filename")]
+        public string FILENAME { get; } = "my-lists.json";
     }
 }
