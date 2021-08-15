ae5f41bff0d75f2e605feaa354bf212d6fb1c8d6
Lindrew <spongebobrafael@gmail.com>
2021-08-15 15:48:04 +0800

    Added list services

---
+++ src/Checkmark/Services/CheckmarkListServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Checkmark.Templates;
+
+namespace Checkmark.Services
+{
+    public class CheckmarkListServices
+    {
+        public void AddList(CheckmarkItemListTemplateJson list)
+        {
+
+        }
+        public void ReadList()
+        {
+
+        }
+        public void UpdateList()
+        {
+            // Should have ability to add more to list and overwrite existing items in list.
+        }
+        public void DeleteList()
+        {
+
+        }
+    }
+}
