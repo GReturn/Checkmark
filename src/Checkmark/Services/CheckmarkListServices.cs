389ffc9e39db6c90855fd9fa8896ebffba1c24a3
Lindrew <spongebobrafael@gmail.com>
2021-08-22 19:49:49 +0800

    Added CheckmarkListServices.cs

---
+++ src/Checkmark/Services/CheckmarkListServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkmark.Services
+{
+    class CheckmarkListServices
+    {
+        /* 
+         * Add here merged CheckmarkFileServices.cs and CheckmarkJsonServices.cs
+         * 
+         * Methods must be relating to list services such as
+         * GetCheckmarkListFromPath()
+         * 
+         * use regions if you decide to separate JSON services.
+        */
+    
+    }
+}
