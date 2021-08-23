011395b01854761f6f662c22682430875f9d7b8e
Lindrew <spongebobrafael@gmail.com>
2021-08-23 23:09:36 +0800

    removed ReadListFileJson() and ReadConfigFileJson()

--- src/Checkmark/Services/CheckmarkFileServices.cs
+++ src/Checkmark/Services/CheckmarkFileServices.cs
@@ -24,27 +24,5 @@ namespace Checkmark.Services
                 File.WriteAllText(pathToFile, json);
             }
         }
-
-        // TODO: make code async. Run before Sharprompt instance in CheckmarkView.cs
-        public static CheckmarkItem ReadListFileJson(CheckmarkConfig config)
-        {
-            var configPath = Path.Combine(config.DIR, config.FILENAME);
-            var jsonFile = string.Empty;
-
-            using (var fileString = File.OpenRead(configPath))
-            using (var streamReader = new StreamReader(fileString, new UTF8Encoding(false)))
-                jsonFile = streamReader.ReadToEnd();
-
-            return CheckmarkJsonServices.Deserialize(jsonFile);
-        }
-        // TODO: Merge with Deserialize<CheckmarkConfig> on CheckmarkJsonServices.cs
-        public static string ReadConfigFileJson()
-        {
-            var pathToFile = Path.Combine(CheckmarkConfig.CheckmarkConfigDirectory, CheckmarkConfig.CheckmarkConfigFileName);
-
-            using (var fileString = File.OpenRead(pathToFile))
-            using (var streamReader = new StreamReader(fileString, new UTF8Encoding(false)))
-                return streamReader.ReadToEnd();
-        }
     }
 }
