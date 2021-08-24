aa581df531e502a89ff4559fe524539670ef85af
Lindrew <spongebobrafael@gmail.com>
2021-08-24 23:36:04 +0800

    Added "view in a table feature"

--- src/Checkmark/UI/CheckmarkView.cs
+++ src/Checkmark/UI/CheckmarkView.cs
@@ -1,6 +1,9 @@
 ﻿using Sharprompt;
 using static System.Console;
 using Checkmark.Services;
+using Checkmark.Templates;
+using ConsoleTables;
+using System.Linq;
 
 namespace Checkmark.UI
 {
@@ -10,12 +13,30 @@ namespace Checkmark.UI
         {
             var viewListChoice = Prompt.Select("How do you want to view your lists?",
                 new[] {
-                    "In a table", // TODO: Feature - Add ConsoleTables: https://github.com/khalidabuhakmeh/ConsoleTables
-                    "No",
-                    "None"
+                    "In a table"
                 });
             var list = CheckmarkMainServices.ReadList();
-            WriteLine($"{list.Priority} | {list.TodoItem} | {list.Status}");
+            MakeListTable(list);
+
+        }
+        private static void MakeListTable(CheckmarkList list)
+        {
+            var table = new ConsoleTable("Id", "Item", "Priority", "Status");
+
+            // TODO: Make list into an array. Reiterate foreach() table.AddRow.
+
+            table.AddRow(1, 2, 3, 4)
+                 .AddRow($"No ID system", $"{list.TodoItem}", $"{list.Priority}", $"{list.Status}");
+
+            table.Write();
+            WriteLine();
+
+            var rows = Enumerable.Repeat(new CheckmarkList(), 10);
+
+            ConsoleTable
+                .From<CheckmarkList>(rows)
+                .Configure(o => o.NumberAlignment = Alignment.Right)
+                .Write(Format.Alternative);
         }
     }
 }
