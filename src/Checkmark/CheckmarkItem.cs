439fed4dac24638a4aa48f99c26f82bdd84a5f6c
Lindrew <spongebobrafael@gmail.com>
2021-08-31 13:10:18 +0800

    Added CheckmarkItem object

---
+++ src/Checkmark/CheckmarkItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Checkmark
+{
+    internal class CheckmarkItem
+    {
+        public string ItemName { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
