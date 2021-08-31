98ad37d613ee25feb0c9c187d16e277cdb214db3
Lindrew <spongebobrafael@gmail.com>
2021-08-31 13:09:33 +0800

    Added records for commands

---
+++ src/Checkmark/Commands/Commands.cs
@@ -0,0 +1,8 @@
+﻿namespace Checkmark.Commands
+{
+    public record Command;
+
+    public record CreateCommand() : Command;
+    public record ReadCommand() : Command;
+    public record DeleteCommand() : Command;
+}
